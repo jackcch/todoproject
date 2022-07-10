90713352ddf77fe229cbdadebbd4136a3ac89d2d
Jack <jackcch8773@gmail.com>
2022-07-10 23:37:13 +0800

    added static for css

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'assets'
 
 LOGIN_URL = '/login/'
 
